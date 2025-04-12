fc8f7b990d12baf05b2072f60a9b5c42f0eec64c
Papa-Sean <smsugrue@gmail.com>
2025-04-12 14:24:26 -0400

    fix: Clean up iframe attributes in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,12 +85,8 @@ export default function Home() {
 						<div className='absolute inset-0 flex items-center justify-center'>
 							<iframe
 								src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.8930883216303!2d-83.0389924!3d42.344802699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d33fa4975911%3A0xad0e213807daa092!2sTocororo!5e0!3m2!1sen!2sus!4v1744477084228!5m2!1sen!2sus'
-								
-								
 								className='border-none w-full h-full'
-								
 								loading='lazy'
-								referrerpolicy='no-referrer-when-downgrade'
 							></iframe>
 						</div>
 					</div>
